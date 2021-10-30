08bb8c3fa41e7d353e990f0d4c1dbe737a97ff57
Ofir <ofir.duchovne@gmail.com>
2021-10-30 23:52:15 +0300

    Ex3

--- cryptoUtils.h
+++ cryptoUtils.h
@@ -43,3 +43,47 @@ int Solve_System(int a,int p,int b, int q) {
     y = inverse(p,q);
     return (a*x*q + b*y*p) % (p*q);
 }
+
+
+
+/*
+    computes x^e mod m
+*/
+unsigned int Exponent( unsigned long long x , unsigned long long e , long long m) 
+{
+    long long exponent = 1;
+    unsigned int ans = x%m;
+    unsigned long long tempExponent = e;
+    int msbFLAG = 0;
+    int index = 1;
+    unsigned int exponentArray[64];
+    int arraySize = 0;
+    
+    /*
+        convert the exponent to an array representing the number in binary form
+    */
+    for(int i=0; i < 64; i++ )
+    {
+        if(tempExponent >> 63) // if we reached the msb start to build the array
+        {
+            msbFLAG++;
+        }
+        if(msbFLAG)
+        {
+            exponentArray[arraySize++] = (tempExponent >> 63); //taking the msb
+        }
+        tempExponent = tempExponent << 1; // moving to the next digit
+    }
+    
+    while(exponent != e) // square and multiply method
+    {
+        ans = (ans * ans ) % m;
+        exponent *= 2;
+        if(exponentArray[index++]) // multiply part (only if the bit is 1)
+        {
+            ans = (ans * x) % m;
+            exponent += 1;
+        }
+    }
+    return ans;
+}
